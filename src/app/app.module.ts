33947748d87abf7989c7436ccdf4199aeb271713
Tosin Orojinmi <tosin@tosins-mbp.home>
2018-02-14 08:44:06 +0000

    starting point

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,20 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyCvE8T2zldYeGMKv0OXVsx2iPpoVDmw28k",
+  authDomain: "afsongs-ed405.firebaseapp.com",
+  databaseURL: "https://afsongs-ed405.firebaseio.com",
+  projectId: "afsongs-ed405",
+  storageBucket: "",
+  messagingSenderId: "1040881740121"
+}
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +28,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
